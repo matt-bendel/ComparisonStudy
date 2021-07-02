53644455424b79452aa5b50531e6b21199ceeded
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2021-07-02 10:30:12 -0400

    Init Commit - CS and utils

---
+++ utils/fastmri/sampling_pattern_gen/GRO_Sampling_Pattern_Generation.m
@@ -0,0 +1,31 @@
+% Code to generate acceleration 4 - GRO sampling mask
+% shastri.19@buckeyemail.osu.edu
+
+%%
+clc; clear all; close all;
+
+%% setting for 360x360 image
+dimension = 360;
+R = 4;
+frames = 1;
+mid_sampling_band = 32; % 32 lines in the middle will be fully sampled
+buffer = 8;
+
+%%
+sampling_lines = (dimension/R);
+
+new_R = dimension/(sampling_lines - mid_sampling_band + buffer);
+
+samp = GoFIX(dimension,frames,new_R);
+
+samp(round(dimension/2) - round(mid_sampling_band/2): round(dimension/2) + round(mid_sampling_band/2)) = 1;
+
+final_acceleration = dimension/nnz(samp)
+
+figure();
+imagesc(samp); xlabel('frames'); ylabel('PE'); %axis('image'); %colormap(c); %colorbar;
+
+[index, val] = find(samp~=0);
+
+csvwrite('gro_r4_mask.csv',samp)
+
