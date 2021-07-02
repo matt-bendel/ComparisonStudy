5753d29ba2e5d58dad9033c5db08afd75bf25ef6
bendel.8 <bendel.8@ece-d01181714s.coeit.osu.edu>
2021-07-02 13:01:34 -0400

    Deleted tests, modified cs

--- utils/fastmri/utils.py
+++ utils/fastmri/utils.py
@@ -11,9 +11,10 @@ from numpy import genfromtxt
 
 import h5py
 import numpy as np
+import os
 
 def generate_gro_mask(accel=4):
-    return genfromtxt('sampling_pattern_gen/gro_r4_mask.csv', delimiter=',')
+    return genfromtxt(os.path.dirname(os.path.abspath(__file__)) + '/gro_r4_mask.csv', delimiter=',')
 
 
 def save_reconstructions(reconstructions: Dict[str, np.ndarray], out_dir: Path):
