683a24b9e230dab55a8835a2ca3758052bf95f0d
Matt Bendel <bendel.8@osu.edu>
2021-07-08 10:13:25 -0400

    changed data path

--- multicoil_convert/convert_to_single.py
+++ multicoil_convert/convert_to_single.py
@@ -63,9 +63,9 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
-# data_path = Path(f'/storage/fastMRI_brain/data/multicoil_{args.data_type}')
-# out_path = Path(f'/storage/fastMRI_brain/data/singlecoil_{args.data_type}')
-data_path = Path(f'/Users/mattbendel/Desktop/Professional/PhD/ComparisonStudy/test_dir/multicoil_{args.data_type}')
-out_path = Path(f'/Users/mattbendel/Desktop/Professional/PhD/ComparisonStudy/test_dir/singlecoil_{args.data_type}')
+data_path = Path(f'/storage/fastMRI_brain/data/multicoil_{args.data_type}')
+out_path = Path(f'/storage/fastMRI_brain/data/singlecoil_{args.data_type}')
+# data_path = Path(f'/Users/mattbendel/Desktop/Professional/PhD/ComparisonStudy/test_dir/multicoil_{args.data_type}')
+# out_path = Path(f'/Users/mattbendel/Desktop/Professional/PhD/ComparisonStudy/test_dir/singlecoil_{args.data_type}')
 
 convert(data_path, out_path)
