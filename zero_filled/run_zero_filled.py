6983e02f7ff4c437d41f98836d789d4f61477e35
bendel.8 <bendel.8@ece-d01181714s.coeit.osu.edu>
2021-07-09 11:07:26 -0400

    Updated directory path for testing

--- zero_filled/run_zero_filled.py
+++ zero_filled/run_zero_filled.py
@@ -42,7 +42,7 @@ def test_zero_filled(data_dir, out_dir):
 
     fastmri.save_reconstructions(reconstructions, out_dir)
 
-data = Path('/storage/fastMRI_brain/data/singlecoil_val')
+data = Path('/storage/fastMRI_brain/data/Matt_preprocessed_data/singlecoil_val')
 #data = Path('/Users/mattbendel/Desktop/Professional/PhD/ComparisonStudy/test_dir/singlecoil_val')
 out = Path('out')
 
